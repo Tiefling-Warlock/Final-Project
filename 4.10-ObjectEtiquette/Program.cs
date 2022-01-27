fbebaebe228e1a721b6f8111e3193a8eaba67314
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-01-27 16:14:48 -0700

    ToString Method Working

--- 4.10-ObjectEtiquette/Program.cs
+++ 4.10-ObjectEtiquette/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+class Account
+{
+    private string name;
+    private decimal balance;
+
+    public override string ToString()
+    {
+        return "Name: " + name + " balance: " + balance;
+    }
+
+    public Account (string inName, decimal inBalance)
+    {
+        name = inName;
+        balance = inBalance;
+    }
+}
+
+class Bank
+{
+    public static void Main()
+    {
+        Account a = new Account("Rob", 25);
+        Console.WriteLine(a);
+    }
+}
