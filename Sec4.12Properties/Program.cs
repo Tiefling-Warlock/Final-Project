889fa90e6f379167bbdc7eec4d906463e28e74af
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-01 23:10:14 -0700

    Using Properties

--- Sec4.12Properties/Program.cs
+++ Sec4.12Properties/Program.cs
@@ -6,13 +6,17 @@ class Bank
     {
         StaffMember s = new StaffMember();
         s.SetAge(21);
-        Console.WriteLine("Age is: " + s.GetAge());        
+        Console.WriteLine("Age is: " + s.GetAge());
+        StaffMember t = new StaffMember();
+        t.Age = 22;
+        Console.WriteLine("Age is: " + t.Age);        
     }
 }
 
 public class StaffMember
 {
     private int age;
+    private int ageValue;
     public int GetAge()
     {
         return this.age;
@@ -22,6 +26,27 @@ public class StaffMember
         if ((inAge > 0) && (inAge < 120))
         {
             this.age = inAge;
+        }
+    }
+    public int Age
+    {
+        set
+        {
+            if((value > 0) && (value < 120))
+            {
+                this.ageValue = value;
+            }
+        }
+        get
+        {
+            return this.ageValue;
+        }
+    }
+    public int AgeInMonths
+    {
+        get
+        {
+            return this.ageValue*12;
         }
     }
 }
