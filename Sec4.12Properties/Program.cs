98cfb55221166e9d23733c39e24d53c9d72026dd
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-01 22:56:46 -0700

    Creating Get and Set methods

--- Sec4.12Properties/Program.cs
+++ Sec4.12Properties/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Bank
+{
+    public static void Main()
+    {
+        StaffMember s = new StaffMember();
+        s.SetAge(21);
+        Console.WriteLine("Age is: " + s.GetAge());        
+    }
+}
+
+public class StaffMember
+{
+    private int age;
+    public int GetAge()
+    {
+        return this.age;
+    }
+    public void SetAge(int inAge)
+    {
+        if ((inAge > 0) && (inAge < 120))
+        {
+            this.age = inAge;
+        }
+    }
+}
