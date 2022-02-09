c5fff369081a1463d0a05e2ae1cc28e3b83369a6
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-08 23:45:17 -0700

    This is a review of enum types and structs

---
+++ review2/Program.cs
@@ -0,0 +1,31 @@
+﻿enum WindowState
+{
+    Quoted,
+    Ordered,
+    Manufactured,
+    Shipped,
+    Installed
+};
+
+struct Window
+{
+    public WindowState state;
+    public double Width;
+    public double Height;
+    public string Description;
+};
+
+class Demonstrations
+{
+    public static void Main()
+    {
+        Window BobsWindow;
+        BobsWindow.Description = "Fadded Paint";
+        Console.WriteLine(BobsWindow.Description);
+        BobsWindow = new Window();
+        BobsWindow.Height = 3;
+        Console.WriteLine($" Bob's window is {BobsWindow.Height} feet tall.");
+        WindowState state;
+        state = WindowState.Manufactured;
+    }
+}
