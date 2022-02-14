7b99f255ab180f89f08cad715b7f1ba84f5c492a
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-13 23:29:49 -0700

    Section 5.1

---
+++ Sect5.1_Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿class DictionaryBank
+{
+    Dictionary<string, IAccount> accountDictionary =
+    new Dictionary<string, IAccount>();
+    public IAccount FindAccount(string name)
+    {
+        if (accountDictionary.ContainsKey(name))
+            return accountDictionary[name];
+        else
+            return null;
+    }
+    public bool StoreAccount(IAccount account)
+    {
+        if (accountDictionary.ContainsKey(account.GetName()))
+            return false;
+        accountDictionary.Add(account.GetName(), account);
+        return true;
+    }
+}
+
+public interface IAccount
+{
+    void PayInFunds(decimal amount);
+    bool WithdrawFunds(decimal amount);
+    decimal GetBalance();
+    string GetName();
+}
