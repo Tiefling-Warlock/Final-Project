90b295fa5038dc445f431032a0fe93bf7a157ec8
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-18 00:52:11 -0700

    Practice sor section 5.3

---
+++ BuisnessObject/Program.cs
@@ -0,0 +1,65 @@
+﻿public class AccountEditTextUI
+{
+    private IAccount account;
+    // public AccountEditTextUI(Account inAccount)
+    // {
+    //     this.account = inAccount;
+    // }
+    public void EditName()
+    {
+        string newName;
+        Console.WriteLine("Name Edit");
+        while (true)
+        {
+            Console.Write("Enter new name : ");
+            newName = Console.ReadLine();
+            string reply;
+            reply = this.account.ValidateName(newName);
+            if (reply.Length == 0)
+            {
+                break;
+            }
+            Console.WriteLine("Invalid name : " + reply);
+        }
+        this.account.SetName(newName);
+    }
+}
+
+public interface IAccount
+{
+    string GetName();
+    string SetName(string inName);
+    string ValidateName(string name);
+}
+public class CustomerAccount
+{
+    private string name;
+    public string GetName()
+    {
+        return this.name;
+    }
+    public static string ValidateName(string name)
+    {
+        if (name == null)
+        {
+            return "Name parameter null";
+        }
+        string trimmedName = name.Trim();
+        if (trimmedName.Length == 0)
+        {
+            return "No text in the name";
+        }
+        return "";
+    }
+    public bool SetName(string inName)
+    {
+        string reply;
+        reply = ValidateName(inName);
+        if (reply.Length > 0)
+        {
+            return false;
+        }
+        this.name = inName.Trim();
+        return true;
+    }
+}
