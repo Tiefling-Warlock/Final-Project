c08bf02a3aa30434b8c569a256e3f706737829e2
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-22 22:41:35 -0700

    Error Handling code from Section 5.5

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class BankException : System.Exception
+{
+    public BankException(string message) :
+    base(message)
+    {
+    }
+}
+public class BankExceptionBadName : System.Exception
+{
+    public BankExceptionBadName(string message) :
+    base(message)
+    {
+    }
+}
+public class BankExceptionBadAddress : System.Exception
+{
+    public BankExceptionBadAddress(string message) :
+    base(message)
+    {
+    }
+
+    class Bank
+    {
+        public static void Main()
+        {
+            Account a;
+            try
+            {
+                a = new Account("Rob", "");
+            }
+            catch (BankExceptionBadName nameException)
+            {
+                Console.WriteLine("Invalid name : " +
+                nameException.Message);
+            }
+            catch (BankExceptionBadAddress addrException)
+            {
+                Console.WriteLine("Invalid address : " +
+                addrException.Message);
+            }
+            catch (System.Exception exception)
+            {
+                Console.WriteLine("System exception : " +
+                exception.Message);
+            }
+
+        }
+    }
+}
