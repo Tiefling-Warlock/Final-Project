09c9501bfc612592950f0dcfdf9b0092b25783cc
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-02-25 02:03:45 -0700

    Progam organization using separate sorce files and namespace

---
+++ Organization/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using CustomerBanking;
+
+class AccountTest
+{
+    public static void Main()
+    {
+        CustomerBanking.Account test;
+        test = new CustomerBanking.Account();
+        test.PayInFunds (50);
+        Console.WriteLine("Balance: " + test.GetBalance());
+    }
+}
