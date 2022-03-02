c2c85390d0b1c10b7f036b33d3c456775cf34098
Danielle Arnesen <ladybug.arnesen@gmail.com>
2022-03-02 00:32:38 -0700

    Working though Chapter 6 part one

---
+++ Homework/Ch6PartOne/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Employee
+{
+    public string FirstName;
+    public string LastName;
+    public string? Salary;
+
+    public string GetName()
+    {
+        return $"{FirstName} {LastName}";
+    }
+
+    public void SetName(string newFirstName, string newLastName)
+    {
+        this.FirstName = newFirstName;
+        this.LastName = newLastName;
+        Console.WriteLine($"Name changed to '{this.GetName()}'");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee employee1 = new Employee();
+        Employee employee2;
+        employee2 = new Employee();
+
+        employee1.FirstName = "Inigo";
+        employee1.LastName = "Montoya";
+        employee1.Salary = "Too Little";
+        IncreaseSalary(employee1);
+        Console.WriteLine($"{employee1.GetName()}: {employee1.Salary}");
+    }
+    static void IncreaseSalary(Employee employee)
+    {
+        employee.Salary = "Enough to survive on";
+    }
+}
